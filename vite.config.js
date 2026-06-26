29d1ba30dd43027f0f4f670223c91c4da04ba7d7
Viktor Kyshenchuk <viktor.kyshenchuk@gmail.com>
2026-06-26 21:05:41 +0300

    vercel joks

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/random/',
+  base: process.env.VERCEL ? '/' : '/random/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
